bc95cb85742383a7de65c7d5fd3edb2d78e109e1
andrew362 <37003497+andrew362@users.noreply.github.com>
2018-04-18 11:59:10 +0200

    prevent script injection

--- script.js
+++ script.js
@@ -2,11 +2,21 @@ var taskList = [];
 var preHtml = '<li class="list-group-item d-flex justify-content-between align-items-center"><p>';
 var postHtml = '</p><span><buttontype="button" class="btn btn-sm btn-danger">&times</button></span></li>';
 
+function removeChar(string) {
+    var newString = '';
+    for(i=0; i < string.length; i++){     
+        if(string[i] !== '<' && string[i] !== '>') {
+            newString += string[i];
+        }
+    }
+    return newString;
+}
 
 function addNewTask() {
     var newTask = $("#newTask").val();
-    if (newTask !== ''){
-        $('#taskList').append(preHtml + newTask + postHtml);
+    var newString = removeChar(newTask);
+    if (newString.trim() !== ''){
+        $('#taskList').append(preHtml + newString + postHtml);
         $("#newTask").val('');
     }
 };
@@ -23,7 +33,7 @@ $(document).keypress(function(event){
 });
 
 $(document).on('click','#taskList li p', function(){
-    console.log('taskli');
+    
     $(this).toggleClass('done');
 });
 
