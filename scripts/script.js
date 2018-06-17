a499b8b65fd39389dd480eefa445d3d91c5e51ed
andrew362 <37003497+andrew362@users.noreply.github.com>
2018-06-17 21:38:15 +0200

    -

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,13 @@ const postHtml = '</p><div><button class="editContent btn btn-sm btn-info">Edit<
 var taskObj = {};
 var taskArray = [];
 
+function pressEnter(func) {
+    $(document).keypress(function (event) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode == '13') func();
+    });
+}
+
 function removeChar(string) {
     var newString = '';
     for (i = 0; i < string.length; i++) {
@@ -21,29 +28,25 @@ function removeChar(string) {
 function addToTaskArray(obj) {
     taskArray.push(obj);
     taskList.append(preHtml + obj.id + '"><p>' + obj.text + postHtml);
-    console.log(taskArray);
 }
 
 function readFromLocalStorage() {
     taskArray = [];
-    for (var i = 0; i < localStorage.length; i++) {
-        var localStorageObj = JSON.parse(localStorage.getItem(localStorage.key(i)));
-        if (localStorageObj.id.indexOf("ToDoList_") != -1){
-            var task = preHtml + localStorage.key(i) + '"><p>' + localStorageObj.text + postHtml;
-            taskList.append(task);
-            taskArray.push(localStorageObj);
-            //console.log($('#taskList li:last').addClass('done'));
-            var temp = $('#taskList li:last');
-            if (localStorageObj.done == 1) {
-                temp.addClass('done');
-            } else if (localStorageObj.done == 0) {
-                temp.removeClass('done');
+        var localStorageObj = JSON.parse(localStorage.getItem("ToDoList"));
+        if(localStorageObj.length > 0){
+            for (var i = 0; i < localStorageObj.length; i++) {
+                var task = preHtml + localStorageObj[i].id + '"><p>' + localStorageObj[i].text + postHtml;
+                taskList.append(task);
+                taskArray.push(localStorageObj[i]);
+                var temp = $('#taskList li:last');
+                if (localStorageObj[i].done == 1) {
+                    temp.addClass('done');
+                } else if (localStorageObj[i].done == 0) {
+                    temp.removeClass('done');
+                }
             }
         }
-    }
-    console.log(taskArray);
 }
-
 
 function toggleTaskDone(task) {
     let taskId = task.dataset.id;
@@ -52,7 +55,6 @@ function toggleTaskDone(task) {
             if (taskArray[i].done == 1) {
                 taskArray[i].done = 0;
                 task.classList.remove('done');
-               // console.log(i, taskArray[i]);
             } else {
                 taskArray[i].done = 1;
                 task.classList.add('done');
@@ -60,7 +62,6 @@ function toggleTaskDone(task) {
             }
         }
     }
-    //console.log(taskArray);
 }
 
 function addNewTask() {
@@ -74,19 +75,13 @@ function addNewTask() {
             done: 0,
             text: newString
         };
-        //addToLocalStorage(taskObj);
         addToTaskArray(taskObj);
         $("#newTask").val('');
     }
 }
 
-
-
-
 //usuwanie tasków z listy
-
 function removeFromList(task) {
-    console.log(task.firstChild.innerText, modalBody);
     modalBody.text("Czy chcesz usunąć zadanie? " + "'" + task.firstChild.innerText + "'");
     modalWindow.modal('show');
     confirmBtn.on('click', function () {
@@ -101,13 +96,13 @@ function removeFromList(task) {
         }
        modalWindow.modal('hide');
        modalBody.text('');
+        console.log(taskArray);
+        return taskArray
     });
     $(document).on('hide.bs.modal',modalWindow, function () {
         confirmBtn.off('click'); //usuwanie listenera na buttonie usuń
     });
 }
-
-
 
 $(document).on('click', '.btn-danger', function (e) {
     let task = this.closest("li");
@@ -115,11 +110,9 @@ $(document).on('click', '.btn-danger', function (e) {
     removeFromList(task);
 });
 
-
 $(document).on('click', '.editContent', function (e) {
     e.stopPropagation();
     let taskID = $(this).closest('li');
-//console.log(taskID);
     for (var i = 0 ; i < taskArray.length; i++){
         if (taskArray[i].id == taskID.data('id') ){
             var index = 0;
@@ -140,48 +133,20 @@ $(document).on('click', '.editContent', function (e) {
     console.log(taskArray);
 });
 
-
-
-
-
-
 //dodawanie nowych tasków
-
 $(document).on('click', '#addTask', addNewTask);
-
 //listener dla 'Enter'
-
-$(document).keypress(function (event) {
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-    if (keycode == '13') addNewTask();
-});
-
+pressEnter(addNewTask);
 //zaznaczenie wykonanych tasków
-
 $(document).on('click', '#taskList li', function () {
-    //$(this).toggleClass('done');
     let task = this.closest("li");
     toggleTaskDone(task);
-
-    //    let taskID = this.closest("li").dataset.id;
-    //    let taskObj = localStorage.getItem(taskID);
-    //    if(taskObj.done == 1){
-    //        $(this).removeClass('done');
-    //        taskObj.done = 0;
-    //        console.log(taskObj);
-    //    } else {
-    //        $(this).addClass('done');
-    //        taskObj.done = 1;
-    //
-    //    }
-    //localStorage.removeItem(taskRow);
 });
 
+//odświeżanie
 $(window).on('beforeunload', function () {
-    localStorage.clear();
-    for (i = 0; i < taskArray.length; i++) {
-        localStorage.setItem(taskArray[i].id, JSON.stringify(taskArray[i]));
-    }
+    localStorage.removeItem("ToDoList");
+    localStorage.setItem("ToDoList", JSON.stringify(taskArray));
 });
 
 readFromLocalStorage();
